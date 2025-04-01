8b5336b1d67ae4173cdb82fab43e9d75c383203f
harshith2303 <harshithe00@gmail.com>
2025-04-01 17:19:16 +0530

    ME_BUILDOUT_QTIFY_V2_MODULE_ONE

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+
+
+import { useEffect, useState } from 'react';
+import styles from './App.css';
+import { fetchGenreList, fetchNewAlbums, fetchSongs, fetchTopAlbums } from './api/api';
+import Section from './components/Section/Section';
+import FAQAccordion from './components/FAQ/FAQAccordion';
+import GenreSection from './components/GenreSection/GenreSection';
+
 
 function App() {
+  const [topAlbums, setTopAlbums] = useState([]);
+  const [newAlbums, setNewAlbums] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const [genreList, setGenreList] = useState([]);
+  
+  const generatedata = async () => {
+    setTopAlbums(await fetchTopAlbums());
+    setNewAlbums(await fetchNewAlbums());
+    setSongs(await fetchSongs());
+    setGenreList(await fetchGenreList());
+  }
+
+
+  useEffect(() => {
+    generatedata();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <NavBar searchData={songs}/>
+      <Hero />
+      <div style={{marginBottom: '30px'}}>
+        <Section data={topAlbums} title="Top Albums" />
+      </div>
+      <div style={{marginBottom: '30px'}}>
+        <Section data={newAlbums} title="New Albums" />
+      </div>
+      <hr className={styles.divider} />
+      <div>
+        <GenreSection data={songs} title="Songs" genreList={genreList} />
+      </div>
+      <hr className={styles.divider} />
+      <FAQAccordion />
     </div>
   );
 }
